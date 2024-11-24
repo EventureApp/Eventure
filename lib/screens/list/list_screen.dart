f4566cdd03664beae919b02c8c8409ea258e1dda
3003809 <noahrichardt04@gmail.com>
2024-11-24 17:13:31 +0100

    fixed list_screen by using provider

--- lib/screens/list/list_screen.dart
+++ lib/screens/list/list_screen.dart
@@ -1,33 +1,33 @@
-import 'package:eventure/models/event.dart';
 import 'package:flutter/material.dart';
-import '../../widgets/event_card.dart';
+import 'package:provider/provider.dart';
+
 import '../../providers/event_provider.dart';
+import '../../widgets/event_card.dart';
 
 class ListScreen extends StatelessWidget {
-   const ListScreen({Key? key}) : super(key: key);
-
-
+  const ListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-     EventProvider provider = new EventProvider();
-     List<Event> events = provider.events;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Events'),
-      ),
-      body: ListView.builder(
-        itemCount: events.length,
-        itemBuilder: (context, index) {
-          final event = events[index];
-          return EventCard(
-            name: event.name,
-            startDate: event.startDate,
-            adress: event.adress,
-            icon: event.icon,
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Events'),
+        ),
+        body: Consumer<EventProvider>(
+          builder: (context, eventProvider, child) {
+            return ListView.builder(
+              itemCount: eventProvider.events.length,
+              itemBuilder: (context, index) {
+                final event = eventProvider.events[index];
+                return EventCard(
+                  name: event.name,
+                  startDate: event.startDate,
+                  adress: event.adress,
+                  icon: event.icon,
+                );
+              },
+            );
+          },
+        ));
   }
 }
