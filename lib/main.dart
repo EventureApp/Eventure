c1ab8cc63c5f0e434f2603230b1fed7ff486ec96
Lenny <lenny-zesewitz@web.de>
2025-01-12 15:16:11 +0100

    goroute erstellt

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:eventure/screens/profile/user_profile.dart';
 
 import 'firebase_options.dart';
 import 'providers/auth_provider.dart';
